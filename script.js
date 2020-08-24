42fbb10feef421f127efef69cbb545715d1dd7f1
jevans63 <jevans63@kent.edu>
2020-08-23 22:09:34 -0400

    timer added

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const TIMER = document.querySelector(".timer");
+const STARTBUTTON = document.querySelector("#start");
 
-TIMER.innerHTML = "02:00";
+var isRunning = false;
+var interval;
+timer = [2,0,0];
+
+//
+function runTimer(){
+    let currentTime = Math.abs(timer[0]) + ":" + timer[1];
+    TIMER.innerHTML = currentTime;
+    timer[2]--;
+    timer[0] = Math.floor((timer[2]/100)/60);
+    timer[1] = Math.floor((timer[2]/100) - (timer[0] * 60));
+}
+//
+function start(){
+    interval = setInterval(runTimer, 10);
+}
+
+STARTBUTTON.addEventListener("click", start, false);
