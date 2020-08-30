86c76a662c71b256192aedf66587ed320c2f03a4
jevans63 <jevans63@kent.edu>
2020-08-30 13:34:43 -0400

    progress bar change
    Now using a CSS based progress bar instead of HTML progress tag

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const STARTBUTTON = document.querySelector("#start");
 const RESETBUTTON = document.querySelector("#reset");
 const SECONDS = document.querySelector("#seconds");
-const PROGRESSBAR = document.querySelector("#progress");
+// const PROGRESSBAR = document.querySelector("#progress");
+const BAR = document.querySelector(".bar")
 const ALERT = document.querySelector(".alert");
 
 var timerStopped = false;
 var interval;
 var timer = 0;
+var max;
 
 //
 function runTimer(){
     if(timer > 0) --timer;
-    PROGRESSBAR.value = timer;
+    // PROGRESSBAR.value = timer;
+    BAR.style.width = (timer/max)*100 + "%";
     if(timer == 0)
         ALERT.innerHTML = "ALERT!";
 }
@@ -19,8 +22,9 @@ function runTimer(){
 //
 function start(){
     timer = SECONDS.value;
-    PROGRESSBAR.value = timer;
-    PROGRESSBAR.max = timer;
+    // PROGRESSBAR.value = timer;
+    // PROGRESSBAR.max = timer;
+    max = timer;
     interval = setInterval(runTimer, 1000);
     RESETBUTTON.removeAttribute("hidden");
     STARTBUTTON.replaceWith(RESETBUTTON);
@@ -31,10 +35,10 @@ function reset(){
     timerStopped = false;
     clearInterval(interval);
     timer = SECONDS.value;
-    PROGRESSBAR.value = timer;
+    // PROGRESSBAR.value = timer;
+    BAR.style.width = "100%";
     RESETBUTTON.replaceWith(STARTBUTTON);
     ALERT.innerHTML = "";
-
 }
 
 STARTBUTTON.addEventListener("click", start, false);
